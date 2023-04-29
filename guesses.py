df84f288417e58f5fbf7be8a08fa2e02defb9e25
fabs-pe <14333@coderacademy.edu.au>
2023-04-29 11:18:16 +1000

    function structure

--- guesses.py
+++ guesses.py
@@ -1,17 +1,19 @@
- 
-# print("This is the start of the program.")
-# time.sleep(5)
-# print("This prints five seconds later.")
-
 user_name = (input("Enter you Name: "))
 user_age = int(input("Enter your age: ")) 
 
-def guesses():
+def welcome_message():
+    print(f"Welcome {user_name} ")
+
+def game_play():
     if user_age >= 15:
         print(f"Welcome {user_name} Please Choose a number between 1-15:  ")
     else:
         print(f"Welcome {user_name} ")
         
-def under_age():
-    if user_age <= 15:
-        print("Please Choose a number between 1-10:")
+def results_list():
+    print("list_test")
+    
+def challenge_game():
+    print("challenge_test")
+    
+
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from guesses import guesses, under_age
+from guesses import welcome_message, game_play, results_list, challenge_game
 
 
 print("Welcome to Guess The Number \n")
+
+welcome_message()
 
 print("Game Rules")
 print("> Enter your name and age.")
@@ -10,5 +12,36 @@ print("> You will have 3 guesses to get the correct number and move on to the ne
 print("> Each round has a larger range, must win pervious to move on")
 print("> Almost forgot!! There is a timer. \n")
 
-guesses()
-under_age()
+
+def menu():
+    print("1. Enter 1 to start the game")
+    print("2. Enter 2 to view your results")
+    print("3. Enter 3 for the daily challenge")
+    print("4. Enter 4 to exit")
+    choice = input("Enter your selection: ")
+    return choice
+
+user_choice = ""
+
+while user_choice != "4":
+    user_choice = menu()
+
+    if (user_choice == "1"):
+        game_play()
+    elif (user_choice == "2"):
+       results_list()
+    elif (user_choice == "3"):
+        challenge_game()
+    elif (user_choice == "4"):
+        continue
+    else:
+        print("Invalid Input")
+
+    input("Press Enter to continue....")
+
+
+print("Thank you for playing, see you soon!")
+
+
+# under_age()
+# countdown(0,0,6)
