66188898650dab54ceeb940661c145211507c0f3
fabs-pe <14333@coderacademy.edu.au>
2023-04-29 21:25:24 +1000

    guess function

--- guesses.py
+++ guesses.py
@@ -1,14 +1,33 @@
+from random import *
+
+
+
 user_name = (input("Enter you Name: "))
 user_age = int(input("Enter your age: ")) 
 
 def welcome_message():
     print(f"Welcome {user_name} , lets get started: ")
+    
+
 
 def game_play():
-    if user_age >= 15:
-        print(f"Welcome {user_name} Please Choose a number between 1-15:  ")
-    else:
-        print(f"Welcome {user_name} ")
+    guess_attempts = 3
+    actual_number_r1 = randint(1, 15)
+    
+    while guess_attempts > 0:
+      guess = int(input("Enter your guess between 1-15: "))
+      print(actual_number_r1)
+      if guess != actual_number_r1:
+        print("Incorrect")
+        guess_attempts -= 1
+        print(f"Remaining attempts: {guess_attempts}")
+      else:
+        print("You guessed correctly. Move to the next round")
+        break
+    print("Game Over")
+
+
+
         
 def results_list():
     print("list_test")
