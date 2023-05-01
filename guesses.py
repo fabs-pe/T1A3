1511852e659f6e4d63818fe282c845bac582f1d7
fabs-pe <14333@coderacademy.edu.au>
2023-05-01 20:09:26 +1000

    game play and challenge functions

--- guesses.py
+++ guesses.py
@@ -11,19 +11,52 @@ def welcome_message():
 
 
 def game_play():
-    guess_attempts = 3
+    guess_attempts_r1 = 3
+    guess_attempts_r2 = 3
+    guess_attempts_r3 = 3
     actual_number_r1 = randint(1, 15)
+    actual_number_r2 = randint(1, 30)
+    actual_number_r3 = randint(1, 50)
+    result_r1 = False
+    result_r2 =False
     
-    while guess_attempts > 0:
+    while guess_attempts_r1 > 0:
       guess = int(input("Enter your guess between 1-15: "))
-      print(actual_number_r1)
+      print(actual_number_r1) #testing only
       if guess != actual_number_r1:
         print("Incorrect")
-        guess_attempts -= 1
-        print(f"Remaining attempts: {guess_attempts}")
+        guess_attempts_r1 -= 1
+        print(f"Remaining attempts: {guess_attempts_r1}")
+      else:
+        print("You guessed correctly. Move to the next round") 
+        result_r1 = True
+        break
+    # print(f"Game Over, The correct number is {actual_number_r1}")
+    
+    
+    while guess_attempts_r2 > 0 and  result_r1:
+      guess = int(input("Enter your guess between 1-30: "))
+      print(actual_number_r2) #testing only
+      if guess != actual_number_r2:
+        print("Incorrect")
+        guess_attempts_r2 -= 1
+        print(f"Remaining attempts: {guess_attempts_r2}")
       else:
         print("You guessed correctly. Move to the next round")
+        result_r2 = True
         break
+    
+    while guess_attempts_r3 > 0 and result_r2:
+      guess = int(input("Enter your guess between 1-50: "))
+      print(actual_number_r3) #testing only
+      if guess != actual_number_r3:
+        print("Incorrect")
+        guess_attempts_r3 -= 1
+        print(f"Remaining attempts: {guess_attempts_r3}")
+      else:
+        print("You guessed correctly.")
+        break
+    
     print("Game Over")
 
 
@@ -33,6 +66,17 @@ def results_list():
     print("list_test")
     
 def challenge_game():
-    print("challenge_test")
+    num_to_guess = [31, 22, 83, 94, 335, 62, 709, 81, 19, 130, 17, 124]
+    print(f"From this list pick one number that i have picked")
+    print(num_to_guess)
+    
+    user_guess = int(input("Whats you guess: "))
+    random_num = sample(num_to_guess,  1)   # Pick a random item from the list
+    
+    if user_guess != random_num:
+        print(f"Sorry, thats incorrect, try again next time. The correct number was  {random_num[0]}")
+    else:
+        print(f"Well done, correct guess")
+
     
 
--- readme.md
+++ readme.md
@@ -8,4 +8,4 @@ Python programming language will be used to create this application. Also i will
 
 ## Features
 
-The user will be asked to input their name and age. The game will then pick a random number for the user to guess within 3 guesses. The range of numbers will be based on the users age. If age  under 15 the range will be 1 -10 if age is over 15 then the ranfe will be 1 -15. The ranges will get larger as the user moves through the rounds. Total of 3 rounds and need to win to move on to next round.
+The user will be asked to input their name and age. The game will then pick a random number for the user to guess within 3 guesses. The range will get larger as the user moves through the rounds. Total of 3 rounds and need to win to move on to next round.
